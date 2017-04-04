75410098257adef7f0d925f5fe0bf197c3d4c530
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2017-04-04 11:41:59 -0400

    updated change_gene_names.sh path

--- change_gene_names.sh
+++ change_gene_names.sh
@@ -8,5 +8,5 @@ do
   # BLAHgenes.tree ---> BLAH
   prefix=`basename $file .tree`
   # -e flag allows multiple commands. s/ means substitution. /g means global, goes all the way through the line
-  cat $file | sed -f ~/Scripts/cleantreenames.sed > $prefix.cleaned.tree 
+  cat $file | sed -f ~/Scripts/phylogenetics_paper/cleantreenames.sed > $prefix.cleaned.tree
 done
